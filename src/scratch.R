00a7ea20c90d0ccbbcad8c29ab248f2f2443dc0f
drekadair <gwyn.case@gmail.com>
2020-05-05 15:22:01 -0700

    rasterizing stuff

--- src/scratch.R
+++ src/scratch.R
@@ -1,14 +1,3 @@
-a.hr.ha <- 27441
-a.hr.m2 <- a.hr.ha*10000
-a.hr.m2
-# Should be 274410000
-
-# Calculate the radius.
-a.hr.r <- sqrt(a.hr.m2/pi)
-a.hr.r
-# radius should be 9345.983943
-
-sqrt(a.hr.ha*10000/pi)
-
-
-class(r.ba.m)
+calculate_lsm(utz.r, what=c('lsm_c_area_mn', 'lsm_c_ca')) %>% 
+  pivot_wider(id_cols=class, names_from=metric, values_from=value) %>% 
+  left_join(bec.levels, by=c('class'='ID'))
