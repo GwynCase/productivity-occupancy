fb9de06bae173ccbdfa48694ff4b8d0ee9898343
Gwyn Case <45114561+GwynCase@users.noreply.github.com>
2020-07-14 09:25:39 -0700

    moved files

--- src/scratch.R
+++ src/scratch.R
@@ -3,16 +3,27 @@ ggplot(ml.tz, aes(x=amt.tz, y=ratio, label=site)) +
   geom_text(hjust='inward', nudge_y = 0.5) +
   theme_classic()
 
-levels(r.f.hsi)
+library(sf)
+library(raster)
+library(rgdal)
 
-genus.pca$x[,1:2] %>% data.frame() %>% rownames_to_column(var='site') %>% right_join(class.area)
+# Import habitat HSI shapefile
+n.hsi <- st_read('C:/Users/gwync/sfuvault/map/external/habitat_model/nesting_sc.shp')
 
+# Set raster extent based on HSI shapefile.
+ext <- extent(n.hsi)
 
-# Plot it.
-ggplot(class.area, aes(x=PC1, y=PC2, label=site, size=high)) +
-  geom_point() +
-  geom_text(hjust='inward', nudge_y = 0.5) +
-  theme_classic()
+# Make an empty raster to populate with values.
+r <- raster(ext, res=c(100, 100))
 
-sample_lsm(r.f.hsi, y=sites.sf, size=r.hr.m, what='lsm_l_contag', 
-           shape='circle')
+n.hsi.levels <- data.frame(ID=c(-10, -2, -1, 0, 1, 2, 3), 
+                           class.name=c('ocean', 'freshwater', 'river', 'nil', 'low', 'moderate', 'high'))
+
+# Populate BEC polygon data onto empty raster grid.
+r.n.hsi <- rasterize(n.hsi, r, 'gridcode')
+
+# Add the levels to the raster.
+levels(r.n.hsi) <- n.hsi.levels
+
+# Save the raster image.
+writeRaster(r.n.hsi, 'C:/Users/gwync/sfuvault/productivity-occupancy/data/processed/nesting_sc.tif', format='GTiff')
