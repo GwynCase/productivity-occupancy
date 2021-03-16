a8d4a47b6fde4aa9077e20ff94159b0e3ed583c3
Gwyn Case <45114561+GwynCase@users.noreply.github.com>
2021-03-15 20:44:15 -0700

    monday

--- src/scratch.R
+++ src/scratch.R
@@ -1,18 +1,25 @@
-zone.counts
+df %>% mutate(size=case_when(
+  genus == 'Catharus' ~ 'S',
+  species == 'pubescens' ~ 'S',
+  species == 'villosus' ~ 'M',
+  genus == 'Lepus' & is.na(size) ~ 'L',
+  genus == 'Neotoma' ~ 'M',
+  genus == 'Rattus' ~ 'M',
+  genus == 'Neotamias' ~ 'S',
+  genus == 'Tamiasciurus' & is.na(size) ~ 'M',
+  TRUE ~ size
+))
 
-test <- zone.counts %>% filter(source == 'C') %>%
-  pivot_wider(names_from=group, values_from=count, values_fill=0) %>% 
-  ungroup() %>% select(!source) %>% 
-  column_to_rownames(var='zone') %>% 
-  chisq.test(., correct=FALSE, simulate.p.value=TRUE)
+df %>% select(class, order, family, genus, species, common, size) %>% distinct() %>% 
+  arrange(class, order, family, genus, species, common, size) %>% view()
 
+df %>% filter(genus == 'Tamiasciurus' & is.na(size)) %>% view()
 
-
-zone.counts %>% group_by(source) %>% nest() %>% 
-  mutate(ch=map(data, function(data) {
-    data %>% pivot_wider(names_from=group, values_from=count, values_fill=0) %>% 
-      column_to_rownames(var='zone') %>% 
-      chisq.test(., correct=FALSE, simulate.p.value=TRUE)
-  })) 
-
-
+df %>% mutate(family=case_when(genus == 'Rattus' & !is.na(comments) & size == 'L' ~ 'Aplodontiidae',
+                               TRUE ~ family),
+              genus=case_when(genus == 'Rattus' & !is.na(comments) & size == 'L' ~ 'Aplodontia',
+                              TRUE ~ genus),
+              species=case_when(genus == 'Rattus' & !is.na(comments) & size == 'L' ~ 'rufa',
+                                TRUE ~ species),
+              common=case_when(genus == 'Rattus' & !is.na(comments) & size == 'L' ~ 'mountain beaver',
+                               TRUE ~ common))
