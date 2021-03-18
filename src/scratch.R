ca88568916dd6d516a1ade8a057873daa0630c13
Gwyn Case <45114561+GwynCase@users.noreply.github.com>
2021-03-17 17:07:10 -0700

    camera checking

--- src/scratch.R
+++ src/scratch.R
@@ -1,17 +1,4 @@
+new.data %>% group_by(site, year=year(datetime)) %>% 
+  summarize(first=min(datetime), last=max(datetime))#, days=difftime(max, min), n=n())
 
-rv.vars <- tribble(
-  ~nest, ~prop.sq.mass, ~prop.sq.count,
-'MTC2019',	0.7172089,	0.5660377,
-'MTF2019',	0.7167476,	0.6153846,
-'RLK2019',	0.7755876,	0.6486486,
-'TCR2019',	0.8112324,	0.6451613,
-'TMC2019',	0.4872569,	0.3200000,
-'UTZ2019',	0.7271142,	0.7600000)
-rv
-
-rv.vars <- camera.sites.2019 %>% select(nest, site, n_fledge) %>% 
-  full_join(rv, by=c('nest'))
-
-rv.vars
-
-lm(n_fledge ~ prop.sq.mass, data=rv.vars) %>% summary()
+grab.some(new.data)
